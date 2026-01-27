d45a5bfbe28a86961827526fd6652ad37d114016
Art'm <kasko.artem45@gmail.com>
2026-01-27 20:12:00 +0300

    Create CountriesService.cs

---
+++ CRUDTests/CountriesServiceTest.cs
@@ -0,0 +1,27 @@
+﻿using ServiceContracts;
+
+namespace CRUDTests;
+
+public class CountriesServiceTest
+{
+    private readonly ICountriesService _countriesService;
+
+    public CountriesServiceTest()
+    {
+        
+    }
+    [Fact]
+    public void Test1()
+    {
+        //Arrange
+        MyMath mm = new MyMath();
+        var input1 = 10;
+        var input2 = 5;
+        var expected = 15;
+        //Act
+        var actual = mm.Add(input1, input2);
+
+        //Assert
+        Assert.Equal(expected, actual);
+    }
+}
