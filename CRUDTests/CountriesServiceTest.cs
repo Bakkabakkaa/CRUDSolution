335a206ee0ec20a8edd5680d0c68e6f7d216a606
Art'm <kasko.artem45@gmail.com>
2026-01-28 00:50:51 +0300

    Create tests

--- CRUDTests/CountriesServiceTest.cs
+++ CRUDTests/CountriesServiceTest.cs
@@ -1,4 +1,6 @@
 ﻿using ServiceContracts;
+using ServiceContracts.DTO;
+using Services;
 
 namespace CRUDTests;
 
@@ -8,20 +10,80 @@ public class CountriesServiceTest
 
     public CountriesServiceTest()
     {
-        
+        _countriesService = new CountriesService();
     }
+    
+    //When CountryAddRequest is null, it should ArgumentNullException
     [Fact]
-    public void Test1()
+    public void AddCountry_NullCountry()
     {
         //Arrange
-        MyMath mm = new MyMath();
-        var input1 = 10;
-        var input2 = 5;
-        var expected = 15;
-        //Act
-        var actual = mm.Add(input1, input2);
-
+        CountryAddRequest? request = null;
         //Assert
-        Assert.Equal(expected, actual);
+        Assert.Throws<ArgumentNullException>(() =>
+        {
+            //Act
+            _countriesService.AddCountry(request);
+        });
     }
+    
+    //When the CountryName is null, it should throw ArgumentException
+    [Fact]
+    public void AddCountry_CountryNameIsNull()
+    {
+        //Arrange
+        CountryAddRequest? request = new CountryAddRequest()
+        {
+            CountryName = null
+        };
+        //Assert
+        Assert.Throws<ArgumentException>(() =>
+        {
+            //Act
+            _countriesService.AddCountry(request);
+        });
+    }
+    
+    //When the CountryName is duplicate, it should throw ArgumentException
+    [Fact]
+    public void AddCountry_DuplicateCpuntryName()
+    {
+        //Arrange
+        CountryAddRequest? request1 = new CountryAddRequest()
+        {
+            CountryName = "USA"
+        };
+        
+        CountryAddRequest? request2 = new CountryAddRequest()
+        {
+            CountryName = "USA"
+        };
+        //Assert
+        Assert.Throws<ArgumentException>(() =>
+        {
+            //Act
+            _countriesService.AddCountry(request1);
+            _countriesService.AddCountry(request2);
+        });
+    }
+    
+    //When you supply proper country name, it should insert (add) the country to the existing list of countries
+    [Fact]
+    public void AddCountry_ProperCountryDetails()
+    {
+        //Arrange
+        CountryAddRequest? request = new CountryAddRequest()
+        {
+            CountryName = "Japan"
+        };
+        
+        //Act
+        CountryResponse response = _countriesService.AddCountry(request);
+        
+        //Assert
+        Assert.True(response.CountryID != Guid.Empty);
+    }
+
+
+    
 }
