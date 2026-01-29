42d94def244bd46f38b85bde806ed7e904c4e697
Art'm <kasko.artem45@gmail.com>
2026-01-30 00:41:14 +0300

    Implementation AddPerson

--- Services/PersonsService.cs
+++ Services/PersonsService.cs
@@ -1,3 +1,4 @@
+using Entities;
 using ServiceContracts;
 using ServiceContracts.DTO;
 
@@ -5,9 +6,48 @@ namespace Services;
 
 public class PersonsService : IPersonsService
 {
+    private readonly List<Person> _persons;
+    private readonly ICountriesService _countriesService;
+
+    public PersonsService()
+    {
+        _persons = new List<Person>();
+        _countriesService = new CountriesService();
+    }
+
+    private PersonResponse ConvertPersonToPersonResponse(Person person)
+    {
+        PersonResponse personResponse = person.ToPersonResponse();
+        personResponse.Country = _countriesService.GetCountryByCountryID(person.CountryID)?.CountryName;
+
+        return personResponse;
+    }
     public PersonResponse AddPerson(PersonAddRequest? personAddRequest)
     {
-        throw new NotImplementedException();
+        // Check if PersonAddRequest is not null
+        if (personAddRequest == null)
+        {
+            throw new ArgumentNullException(nameof(personAddRequest));
+        }
+        
+        // Validate PersonName
+        if (string.IsNullOrEmpty(personAddRequest.PersonName))
+        {
+            throw new ArgumentException("Personname can't be blank");
+        }
+        
+        // Convert personAddRequest into Person type
+        Person person = personAddRequest.ToPerson();
+        
+        // Generate PersonID
+        person.PersonID = Guid.NewGuid();
+        
+        // Add person object to persons list 
+        _persons.Add(person);
+
+        // Convert the Person object into PersonResponse type
+        return ConvertPersonToPersonResponse(person);
+        
     }
 
     public List<PersonResponse> GetAllPersons()
