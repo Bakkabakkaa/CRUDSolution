ac7f56554a5f716e3c8805cc3089de2ea9d25e64
Art'm <kasko.artem45@gmail.com>
2026-01-31 01:16:35 +0300

    add tests for GetPersonByPersonID method

--- CRUDTests/PersonsServiceTests.cs
+++ CRUDTests/PersonsServiceTests.cs
@@ -9,10 +9,12 @@ namespace CRUDTests;
 public class PersonsServiceTests
 {
     private readonly IPersonsService _personsService;
+    private readonly ICountriesService _countriesService;
 
     public PersonsServiceTests()
     {
         _personsService = new PersonsService();
+        _countriesService = new CountriesService();
     }
 
     #region AddPerson
@@ -70,5 +72,50 @@ public class PersonsServiceTests
         Assert.Contains(person_response_from_add, person_list);
     }
     
+    #endregion
+
+    #region GetPersonByPersonID
+
+    [Fact]
+    // If we supply as PersonID, it should return null as PersonResponse
+    public void GetPersonByPersonID_NullPersonID()
+    {
+        // Arrange
+        Guid? personID = null;
+        
+        // Act
+        PersonResponse? person_response_from_get = _personsService.GetPersonByPersonID(personID);
+        
+        // Assert 
+        Assert.Null(person_response_from_get);
+    }
+
+    [Fact]
+    // If we supply a valid person id, it should return the valid
+    // person details as PersonResponse object
+    public void GetPersonByPersonID_WithPersonID()
+    {
+        // Arrange
+        CountryAddRequest country_request = new CountryAddRequest()
+        {
+            CountryName = "Canada"
+        };
+        CountryResponse country_response = _countriesService.AddCountry(country_request);
+
+        // Act
+        PersonAddRequest person_request = new PersonAddRequest()
+        {
+            PersonName = "Person", Email = "person@example.com",
+            Address = "sample address", CountryID = country_response.CountryID,
+            Gender = GenderOptions.Male, DateOfBirth = DateTime.Parse("2000-01-01"),
+            ReceiveNewsLetters = true
+        };
+
+        PersonResponse person_response_from_add = _personsService.AddPerson(person_request);
+        PersonResponse? person_response_from_get = _personsService.GetPersonByPersonID(person_response_from_add.PersonID);
+
+        // Assert
+        Assert.Equal(person_response_from_add, person_response_from_get);
+    }
     #endregion
 }
