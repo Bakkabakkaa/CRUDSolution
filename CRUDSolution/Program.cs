3f7026c2f8d257a4cf938c7fb4e509c20cee60a7
Art'm <kasko.artem45@gmail.com>
2026-02-04 23:41:13 +0300

    Register ICountriesService and IPersonsService

--- CRUDSolution/Program.cs
+++ CRUDSolution/Program.cs
@@ -1,5 +1,14 @@
+using ServiceContracts;
+using Services;
+
 var builder = WebApplication.CreateBuilder(args);
+
 builder.Services.AddControllersWithViews();
+
+// Add services into IoC container
+builder.Services.AddSingleton<ICountriesService, CountriesService>();
+builder.Services.AddSingleton<IPersonsService, PersonsService>();
+
 var app = builder.Build();
 
 if (builder.Environment.IsDevelopment())
