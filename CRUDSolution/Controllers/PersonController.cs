e3a06f320d27cf7757a12d5bb7c08b8b7f2e9fe3
Art'm <kasko.artem45@gmail.com>
2026-02-05 00:49:31 +0300

    Add Index endpoint

--- CRUDSolution/Controllers/PersonController.cs
+++ CRUDSolution/Controllers/PersonController.cs
@@ -1,13 +1,23 @@
 using Microsoft.AspNetCore.Mvc;
+using ServiceContracts;
+using ServiceContracts.DTO;
 
 namespace CRUDSolution.Controllers;
 
 public class PersonController : Controller
 {
+    private readonly IPersonsService _personsService;
+
+    public PersonController(IPersonsService personsService)
+    {
+        _personsService = personsService;
+    }
     [Route("person/index")]
     [Route("/")]
     public IActionResult Index()
     {
-        return View();
+        List<PersonResponse> persons = _personsService.GetAllPersons();
+        
+        return View(persons);
     }
 }
