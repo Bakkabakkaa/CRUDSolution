d57e7f2848334bed5f8b0777f573e2811a47d502
Art'm <kasko.artem45@gmail.com>
2026-02-15 22:03:37 +0300

    Create Repositories

---
+++ Repositories/CountriesRepository.cs
@@ -0,0 +1,27 @@
+﻿using Entities;
+using RepositoryContracts;
+
+namespace Repositories;
+
+public class CountriesRepository : ICountriesRepository
+{
+    public Task<Country> AddCountry(Country country)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Country>> GetAllCountries()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Country?> GetCountryByCountryID(Guid countryID)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<Country?> GetCountryByCountryName(string countryName)
+    {
+        throw new NotImplementedException();
+    }
+}
