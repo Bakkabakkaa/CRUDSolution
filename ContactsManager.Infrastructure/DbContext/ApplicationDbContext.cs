26881ea5c580c7f3e222325d3167fc6b7c99561f
Art'm <kasko.artem45@gmail.com>
2026-03-02 01:01:33 +0300

    Creating Models

--- ContactsManager.Infrastructure/DbContext/ApplicationDbContext.cs
+++ ContactsManager.Infrastructure/DbContext/ApplicationDbContext.cs
@@ -1,9 +1,11 @@
+using ContactsManager.Core.Domain.IdentityEntities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
 
 namespace Entities;
 
-public class ApplicationDbContext :DbContext
+public class ApplicationDbContext : IdentityDbContext<ApplicationUser, ApplicationRole, Guid>
 {
     public virtual DbSet<Country> Countries { get; set; }
     public virtual DbSet<Person> Persons { get; set; }
