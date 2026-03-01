b0c65f58d558960d74a97b0b0daffc4327d32976
Art'm <kasko.artem45@gmail.com>
2026-03-01 21:21:24 +0300

    Migration of the project to a clean architecture

---
+++ ContactsManager.Core/Services/PersonsSorterService.cs
@@ -0,0 +1,87 @@
+using Microsoft.Extensions.Logging;
+using RepositoryContracts;
+using ServiceContracts;
+using ServiceContracts.DTO;
+using ServiceContracts.Enums;
+using Serilog;
+namespace Services;
+
+public class PersonsSorterService : IPersonsSorterService
+{
+    private readonly IPersonsRepository _personsRepository;
+    private readonly ILogger<PersonsSorterService> _logger;
+    private readonly IDiagnosticContext _diagnosticContext;
+    public PersonsSorterService(IPersonsRepository personsRepository, ILogger<PersonsSorterService> logger,
+        IDiagnosticContext diagnosticContext)
+    {
+        _personsRepository = personsRepository;
+        _logger = logger;
+        _diagnosticContext = diagnosticContext;
+    }
+    
+    public async Task<List<PersonResponse>> GetSortedPersons(List<PersonResponse> allPerson, string sortBy, SortOrderOptions sortOrder)
+    {
+        _logger.LogInformation("GetSortedPersons of PersonsGetterService");
+            
+        if (string.IsNullOrEmpty(sortBy))
+        {
+            return allPerson;
+        }
+
+        List<PersonResponse> sortedPerson = (sortBy, sortOrder)
+            switch
+            {
+                (nameof(PersonResponse.PersonName), SortOrderOptions.ASC) =>
+                    allPerson.OrderBy(temp => temp.PersonName, StringComparer.OrdinalIgnoreCase).ToList(),
+
+                (nameof(PersonResponse.PersonName), SortOrderOptions.DESC) =>
+                    allPerson.OrderByDescending(temp => temp.PersonName, StringComparer.OrdinalIgnoreCase).ToList(),
+
+                (nameof(PersonResponse.Email), SortOrderOptions.ASC) =>
+                    allPerson.OrderBy(temp => temp.Email, StringComparer.OrdinalIgnoreCase).ToList(),
+
+                (nameof(PersonResponse.Email), SortOrderOptions.DESC) =>
+                    allPerson.OrderByDescending(temp => temp.Email, StringComparer.OrdinalIgnoreCase).ToList(),
+
+                (nameof(PersonResponse.DateOfBirth), SortOrderOptions.ASC) =>
+                    allPerson.OrderBy(temp => temp.DateOfBirth).ToList(),
+
+                (nameof(PersonResponse.DateOfBirth), SortOrderOptions.DESC) =>
+                    allPerson.OrderByDescending(temp => temp.DateOfBirth).ToList(),
+
+                (nameof(PersonResponse.Age), SortOrderOptions.ASC) =>
+                    allPerson.OrderBy(temp => temp.Age).ToList(),
+
+                (nameof(PersonResponse.Age), SortOrderOptions.DESC) =>
+                    allPerson.OrderByDescending(temp => temp.Age).ToList(),
+
+                (nameof(PersonResponse.Gender), SortOrderOptions.ASC) =>
+                    allPerson.OrderBy(temp => temp.Gender, StringComparer.OrdinalIgnoreCase).ToList(),
+
+                (nameof(PersonResponse.Gender), SortOrderOptions.DESC) =>
+                    allPerson.OrderByDescending(temp => temp.Gender, StringComparer.OrdinalIgnoreCase).ToList(),
+
+                (nameof(PersonResponse.Country), SortOrderOptions.ASC) =>
+                    allPerson.OrderBy(temp => temp.Country, StringComparer.OrdinalIgnoreCase).ToList(),
+
+                (nameof(PersonResponse.Country), SortOrderOptions.DESC) =>
+                    allPerson.OrderByDescending(temp => temp.Country, StringComparer.OrdinalIgnoreCase).ToList(),
+
+                (nameof(PersonResponse.Address), SortOrderOptions.ASC) =>
+                    allPerson.OrderBy(temp => temp.Address, StringComparer.OrdinalIgnoreCase).ToList(),
+
+                (nameof(PersonResponse.Address), SortOrderOptions.DESC) =>
+                    allPerson.OrderByDescending(temp => temp.Address, StringComparer.OrdinalIgnoreCase).ToList(),
+
+                (nameof(PersonResponse.ReceiveNewsLetters), SortOrderOptions.ASC) =>
+                    allPerson.OrderBy(temp => temp.ReceiveNewsLetters).ToList(),
+
+                (nameof(PersonResponse.ReceiveNewsLetters), SortOrderOptions.DESC) =>
+                    allPerson.OrderByDescending(temp => temp.ReceiveNewsLetters).ToList(),
+
+                _ => allPerson
+            };
+
+        return sortedPerson;
+    }
+}
